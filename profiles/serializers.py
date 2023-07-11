d52b37344540270e9a0c2d8251042ccf2a37f99e
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-11 09:51:34 +0000

    apply restrictions and permissions to profile owners

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,12 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
 
     class Meta:
         model = Profile
@@ -14,5 +20,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             'updated_at',
             'name',
             'content',
-            'image'
+            'image',
+            'is_owner'
         ]
