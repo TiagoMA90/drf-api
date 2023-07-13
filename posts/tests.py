f483d23107cf59d0aec528d9d7125b5a13ad0301
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-13 04:19:18 +0000

    make automated tests for posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='user', password='test')
+    
+    def test_can_list_posts(self):
+        tiago = User.objects.get(username='user')
+        Post.objects.create(owner=tiago, title='title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='user', password='test')
+        response = self.client.post('/post/', {'title': 'title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_not_logged_in_cant_creat_post(self):
+        response = selfclient.post('/posts/', {'title': 'title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
