9c550e3336b45c5152bb8dcec7490b14df2bda13
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-15 20:07:55 +0000

    add extra fields to Posts

--- posts/views.py
+++ posts/views.py
@@ -5,10 +5,6 @@ from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
-    """
-    List posts or create a post if logged in
-    The perform_create method associates the post with the logged in user.
-    """
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
@@ -18,9 +14,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Retrieve a post and edit or delete it if you own it.
-    """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.all()
--- profiles/views.py
+++ profiles/views.py
@@ -5,18 +5,11 @@ from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    """
-    List all profiles.
-    No create view as profile creation is handled by django signals.
-    """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
-    """
-    Retrieve or update a profile if you're the owner.
-    """
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
