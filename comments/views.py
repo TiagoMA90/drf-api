0f6056d0a17c67d469d1516d5c6dfdcf53f786eb
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-16 20:02:32 +0000

    apply filterset_field to comments

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer
@@ -14,7 +15,12 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_field = [
+        'post',
+    ]
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
