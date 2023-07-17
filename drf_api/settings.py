43d0a75b97fa550867c1100a15e1dee9c235c30f
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-17 16:06:08 +0000

    check solution from drf-api source ad apply, remove statocfiles_storage, test for heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -196,7 +196,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
