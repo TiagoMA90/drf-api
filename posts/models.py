cd998ced4e32831adb46818dc2b08fb6d592fd1a
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-17 14:56:32 +0000

    check solution from drf-api source ad apply

--- posts/models.py
+++ posts/models.py
@@ -3,14 +3,6 @@ from django.contrib.auth.models import User
 
 # Post Model
 class Post(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_post_u0loxd', blank=True
-    )
     image_filter_choices = [
         ('_1977', '1977'),
         ('brannan', 'Brannan'),
@@ -27,6 +19,14 @@ class Post(models.Model):
         ('walden', 'Walden'),
         ('xpro2', 'X-pro II')
     ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
     )
