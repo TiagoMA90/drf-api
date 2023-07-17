671f06deff43d7be5cb51e602d733d12d0b607b5
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-17 17:43:15 +0000

    try to fix broken render in heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST'), '8000-tiagoma90-drfapi-k92kxve32sf.ws-eu101.gitpod.io', 'localhost', 'djangorestframework-api.herokuapp.com', 'djangorestframework-api-38c4a098777a.herokuapp.com']
+ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST'), '8000-tiagoma90-drfapi-jgx7i417bjb.ws-eu101.gitpod.io', 'localhost', 'djangorestframework-api.herokuapp.com', 'djangorestframework-api-38c4a098777a.herokuapp.com']
 
 
 # Application definition
--- env.py
+++ env.py
@@ -2,5 +2,5 @@ import os
 
 os.environ['CLOUDINARY_URL'] = 'cloudinary://572555385744258:0COw3jIp0nWL_2Lzjn7KTeGgihM@dmbdqco85'
 os.environ.setdefault("SECRET_KEY", "hadouken")
-os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgres://xfoonmbu:8JwqKT7lL451J1EhyDMRH5BZEoYoehB5@trumpet.db.elephantsql.com/xfoonmbu"
+# os.environ['DEV'] = '1'
