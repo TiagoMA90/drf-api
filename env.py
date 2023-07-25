b77fbe21380c11adad27f0d90fd233693e8ac6e0
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-07-25 08:43:34 +0000

    remove env

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['DATABASE_URL'] = "postgres://xfoonmbu:8JwqKT7lL451J1EhyDMRH5BZEoYoehB5@trumpet.db.elephantsql.com/xfoonmbu"
-os.environ['CLOUDINARY_URL'] = 'cloudinary://572555385744258:0COw3jIp0nWL_2Lzjn7KTeGgihM@dmbdqco85'
-os.environ.setdefault("SECRET_KEY", "hadouken")
-os.environ['DEV'] = '1'
