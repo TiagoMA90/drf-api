4f7a615df5ad6caae1f65d8c44745a2675dc1373
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-20 15:04:21 +0000

    remove and eliminate reports

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,4 @@ from posts import views
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view())
-    path('posts/<int:post_id>/report/', views.ReportCreate.as_view()),
 ]
