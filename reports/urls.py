62b76d32453cddedc29eda654911dc9efa52c798
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-27 20:49:18 +0000

    update reports urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from reports import views
+from . import views
 
 urlpatterns = [
-    path('report/', views.ReportList.as_view()),
-    path('report/<int:pk>/', views.ReportDetail.as_view()),
+    path('reports/', views.ReportList.as_view()),
+    path('reports/<int:pk>/', views.ReportDetail.as_view()),
 ]
