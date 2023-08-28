2431e6484424701d1c2de0ae67436b0593e994ad
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-28 22:53:43 +0000

    delete reports

--- reports/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from reports import views
-
-urlpatterns = [
-    path('reports/', views.ReportList.as_view()),
-    path('reports/<int:pk>/', views.ReportDetail.as_view()),
-]
