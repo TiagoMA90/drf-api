867d943692422ba07bf3db96a87c32701ade40e7
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-28 22:37:25 +0000

    commit to test perform_create from views.py in reports

--- reports/views.py
+++ reports/views.py
@@ -6,6 +6,11 @@ class ReportList(generics.ListCreateAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
     permission_classes = [permissions.IsAuthenticated]
+    # Test perform_create and request data fro post_id
+    def perform_create(self, serializer):
+        post_id = self.request.data.get('post')
+        post = Post.objects.get(pk=post_id)
+        serializer.save(reporter=self.request.user, post=post)
 
 class ReportDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Report.objects.all()
