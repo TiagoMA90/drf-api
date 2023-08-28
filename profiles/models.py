3e27ead08dcbdfe00caab5d378f3584fb568b80e
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-28 16:52:05 +0000

    apply logout to delete_user

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.db.models.signals import post_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.contrib.auth import logout
 
 # Profile Model
 class Profile(models.Model):
@@ -30,6 +31,8 @@ post_save.connect(create_profile, sender=User)
 
 def delete_user(sender, instance, **kwargs):
     if instance.owner:
-        instance.owner.delete()
+        user = instance.owner
+        logout(user)
+        user.delete()
 
 post_delete.connect(delete_user, sender=Profile)
