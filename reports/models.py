04f6a06a27678a22343349fabe1f2cbbca777eca
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-28 12:22:08 +0000

    apply changes to urls.py and move the REPORT_CHOICES inside og Report in models.py

--- reports/models.py
+++ reports/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
-REPORT_CHOICES = [
-    ('spam_flame', 'Spamming and Flaming'),
-    ('inappropriate', 'Inappropriate Content'),
-    ('other', 'Other'),
-]
-
 class Report(models.Model):
+    REPORT_CHOICES = [
+        ('spam_flame', 'Spamming and Flaming'),
+        ('inappropriate', 'Inappropriate Content'),
+        ('other', 'Other'),
+    ]
+
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     reason = models.CharField(max_length=32, choices=REPORT_CHOICES)
@@ -19,4 +19,3 @@ class Report(models.Model):
 
     def __str__(self):
         return f"{self.reporter.username} reported '{self.post.title}' for {self.get_reason_display()}"
-
