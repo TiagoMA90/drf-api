1daf94308334eda243a0af9056d4f83e7ea131e0
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-29 23:24:11 +0000

    rewrite typo for urls & views for reports

--- reports/urls.py
+++ reports/urls.py
@@ -3,5 +3,5 @@ from reports import views
 
 urlpatterns = [
     path('reports/', views.ReportList.as_view()),
-    path('reports/<int:pk>/', views.ReportPostView.as_view()),
+    path('reports/<int:pk>/', views.ReportDetail.as_view()),
 ]
