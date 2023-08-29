da3b874a7ce407a9d65f4d9c414010892d0ba126
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-29 16:13:03 +0000

    rebuild Report similar to Like

---
+++ reports/serializers.py
@@ -0,0 +1,24 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from reports.models import Report
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Report
+        fields = [
+            'id',
+            'created_at',
+            'owner',
+            'post'
+            ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
