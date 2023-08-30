06318ed811849c796221b418969c8ad5a0dd7180
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-08-30 15:32:16 +0000

    apply def logout_route from main views.py into profiles views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,11 @@
 from django.db.models import Count
-from rest_framework import generics
-from rest_framework import filters
+from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-
 
 class ProfileList(generics.ListAPIView):
     queryset = Profile.objects.annotate(
@@ -30,7 +30,6 @@ class ProfileList(generics.ListAPIView):
         'owner__followed__created_at',
     ]
 
-
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
@@ -40,11 +39,35 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
 
-
+# TESTING---------------------------------------------------------------
 class ProfileDelete(generics.DestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-    def ProfileEliminate(self, instance):
-        instance.delete()
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
+# TESTING---------------------------------------------------------------
