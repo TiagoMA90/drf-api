7689c0ff4565c5901810edf92fc489f1ae0de767
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-09-04 21:23:44 +0000

    apply followed_id ro followers serializers.py

--- followers/serializers.py
+++ followers/serializers.py
@@ -6,11 +6,12 @@ from .models import Follower
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     followed_name = serializers.ReadOnlyField(source='followed.username')
+    followed_id = serializers.ReadOnlyField(source='followed.profile.id') # testing followed_id
 
     class Meta:
         model = Follower
         fields = [
-            'id', 'owner', 'created_at', 'followed', 'followed_name'
+            'id', 'owner', 'created_at', 'followed', 'followed_name', 'followed_id' #testing followed_id
         ]
 
     def create(self, validated_data):
