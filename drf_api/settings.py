3bf028772e0548692b68057f7543365f7735c6b8
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-09-23 16:51:34 +0000

    test destroy method

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
-    '8000-tiagoma90-drfapi-nmzjwaqpe2q.ws-eu104.gitpod.io',
+    '8000-tiagoma90-drfapi-ksn9j3i7kjo.ws-eu104.gitpod.io',
     'djangorestframework-api-38c4a098777a.herokuapp.com',
     'localhost',
 ]
